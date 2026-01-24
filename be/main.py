d27f70e70890c468e803f054fa6d89ed9c3b4333
StephenJP <stephen.j.pratama@gmail.com>
2026-01-24 16:24:25 +0700

    test connection

---
+++ be/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, UploadFile, File
+
+app = FastAPI()
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    # You can access metadata here
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type,
+        "size_in_bytes": file.size # Note: .size might not be available in all versions immediately without reading, but generic metadata is.
+    }
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
